b9f48f54fe641aeeb7c541091fd3883b455ae5dc
hnagubhai <86932619+hnagubhai@users.noreply.github.com>
2023-02-22 21:20:12 -0600

    Delete src directory

--- src/AgeComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class AgeComparator implements Comparator<Student> {
-    @Override
-    public int compare(Student s1, Student s2) {
-        return s1.getAge() - s2.getAge();
-    }
-}
--- src/SubjectComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class SubjectComparator implements Comparator<Student> {
-    @Override
-    public int compare(Student s1, Student s2) {
-        return s1.getSubject().compareTo(s2.getSubject());
-    }
-}
